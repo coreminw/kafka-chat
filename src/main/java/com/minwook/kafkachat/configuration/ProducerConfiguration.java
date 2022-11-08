aac81ad6614bf1159379aff26faa0636b21a2f2e
BIC-COREMINW\user <coreminw@penta.co.kr>
2022-11-08 14:54:52 +0900

    [ADD] producerConfiguration 추가

--- src/main/java/com/minwook/kafkachat/configuration/ProducerConfiguration.java
+++ src/main/java/com/minwook/kafkachat/configuration/ProducerConfiguration.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @EnableKafka
 @Configuration
-public class ProducerConfiguration {
+public class ProducerConfiguration { //producer는 TOPIC에 메시지를 작성한다.
 
     @Bean
     public ProducerFactory<String, Message> producerFactory(){
@@ -25,14 +25,17 @@ public class ProducerConfiguration {
     @Bean
     public Map<String, Object> producerConfigurations() {
         Map<String, Object> configurations = new HashMap<>();
-        configurations.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.KAFKA_BROKER);
+        configurations.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.KAFKA_BROKER); //BOOTSTRAP_SERVERS_CONFIG는 Kafka가 실행되는 주소를 설정을 한다.
+
+        //KEY_SERIALIZER_CLASS_CONFIG와 VALUE_SERIALIZER_CLASS_CONFIG는 Kafka로 보내는 데이터의 키와 값을 직렬화한다.
+        //문자열을 넘길땐 StringSerializer.class를, JSON 데이터를 넘길 땐 JsonSerializer.class를 적어주면 된다.
         configurations.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configurations.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return configurations;
     }
 
     @Bean
-    public KafkaTemplate<String, Message> kafkaTemplate(){ //KafkaTemplate을 통해 TOPIC에 메시지를 보낼 수 있음
+    public KafkaTemplate<String, Message> kafkaTemplate(){ //KafkaTemplate을 통해 TOPIC에 메시지를 보낼 수 있다.
         return new KafkaTemplate<>(producerFactory());
     }
 }
